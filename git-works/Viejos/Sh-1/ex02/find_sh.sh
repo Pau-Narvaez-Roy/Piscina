1eebb3896f281e57497905223dea15a5cf785471
ptitchat <pau.roy.narvaez@gmail.com>
2026-05-24 21:13:08 +0200

    cambio la estructura de los ejercicios

--- git-works/Viejos/Sh-1/ex02/find_sh.sh
+++
@@ -1,2 +0,0 @@
-#ls -R | grep .sh | sed 's/\.sh$//'
-find . -type f -name "*.sh" -exec basename {} .sh \;
--- git-works/Viejos/Sh-1/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/passwd | grep -v '^#' | sed -n '1-2p' | cur -d: -f1 | rev | sort -rn  "${FT_LINE1}.${FT_LINE2}" | paste -sd. - | sed 's/$/./' 
